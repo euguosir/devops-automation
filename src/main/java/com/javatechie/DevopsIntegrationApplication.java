07055772ec5e19c3aac956d677aa07aced0fcc81
euguosir <42060748+euguosir@users.noreply.github.com>
2022-09-26 22:23:11 +0800

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome : ) release branch";
+		return "welcome test";
 	}
 
 	public static void main(String[] args) {
