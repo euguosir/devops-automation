b79e2ca8d49baf15fb3eaaa6592bf37bb9201e9f
euguosir <42060748+euguosir@users.noreply.github.com>
2022-10-30 20:37:17 +0800

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "version vx.x.x";
+		return "version v1.0.3";
 	}
 
 	public static void main(String[] args) {
